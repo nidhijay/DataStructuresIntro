74f3c7b6e39e9430ca8bf8176e7f6a9b5ecc4838
Nidhi <p.nidhi2830@gmail.com>
2024-08-08 11:00:19 -0500

    done code

--- LinkedList.js
+++ LinkedList.js
@@ -1,1 +1,74 @@
-// create a class called LinkedList. It should have a head and tail property. It should have an instance method called append that will append to the linked list a node instance. Also a property called  insertAt that will insert a node at a given index
+/* create a class called LinkedList. It should have a head and tail property. It should have an instance method called
+ append that will append to the linked list a node instance. Also a property called  insertAt that will insert a node at a 
+given index */
+
+class Node {
+    constructor(value) {
+      this.value = value;
+      this.next = null;
+    }
+  }
+  
+  class LinkedList {
+    constructor() {
+      this.head = null;
+      this.tail = null;
+    }
+  
+    append(value) {
+      const newNode = new Node(value);
+      if (!this.head) {
+        this.head = newNode;
+        this.tail = newNode;
+      } else {
+        this.tail.next = newNode;
+        this.tail = newNode;
+      }
+    }
+  
+    insertAt(index, value) {
+      if (index < 0) {
+        throw new Error("Index must be a non-negative integer");
+      }
+  
+      const newNode = new Node(value);
+  
+      if (index === 0) {
+        newNode.next = this.head;
+        this.head = newNode;
+        if (!this.tail) {
+          this.tail = newNode;
+        }
+        return;
+      }
+  
+      let current = this.head;
+      let currentIndex = 0;
+  
+      while (current && currentIndex < index - 1) {
+        current = current.next;
+        currentIndex += 1;
+      }
+  
+      if (!current) {
+        throw new Error("Index out of range");
+      }
+  
+      newNode.next = current.next;
+      current.next = newNode;
+  
+      if (!newNode.next) {
+        this.tail = newNode;
+      }
+    }
+  }
+  
+  // Example usage:
+  const ll = new LinkedList();
+  ll.append(1);
+  ll.append(2);
+  ll.append(3);
+  ll.insertAt(1, 4); // Linked list becomes 1 -> 4 -> 2 -> 3
+  
+  console.log(JSON.stringify(ll, null, 2));
+  
--- Stack.js
+++ Stack.js
@@ -1,1 +1,50 @@
 // Create a class called Stack. It should have a method to push, pop and peek
+
+class Stack {
+    constructor() {
+      this.items = [];
+    }
+  
+    // Method to push an element to the stack
+    push(element) {
+      this.items.push(element);
+    }
+  
+    // Method to pop an element from the stack
+    pop() {
+      if (this.isEmpty()) {
+        return "Stack is empty";
+      }
+      return this.items.pop();
+    }
+  
+    // Method to peek the top element of the stack
+    peek() {
+      if (this.isEmpty()) {
+        return "Stack is empty";
+      }
+      return this.items[this.items.length - 1];
+    }
+  
+    // Method to check if the stack is empty
+    isEmpty() {
+      return this.items.length === 0;
+    }
+  
+    // Method to get the size of the stack
+    size() {
+      return this.items.length;
+    }
+  }
+  
+  // Example usage:
+  const stack = new Stack();
+  stack.push(10);
+  stack.push(20);
+  stack.push(30);
+  
+  console.log(stack.peek()); 
+  console.log(stack.pop());  
+  console.log(stack.peek()); 
+  console.log(stack.size()); 
+  
